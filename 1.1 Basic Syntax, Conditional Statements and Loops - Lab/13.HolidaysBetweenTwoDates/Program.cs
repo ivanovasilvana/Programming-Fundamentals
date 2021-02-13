20c6095e6b4c4528c4259de51d210f29432c13a8
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-13 11:54:55 +0200

    13.HolidaysBetweenTwoDates

--- 1.1 Basic Syntax, Conditional Statements and Loops - Lab/13.HolidaysBetweenTwoDates/Program.cs
+++ 1.1 Basic Syntax, Conditional Statements and Loops - Lab/13.HolidaysBetweenTwoDates/Program.cs
@@ -7,12 +7,12 @@ namespace _13.HolidaysBetweenTwoDates
         static void Main(string[] args)
         {
             var startDate = DateTime.ParseExact(Console.ReadLine(),
-    "dd.m.yyyy", CultureInfo.InvariantCulture);
+                 "d.M.yyyy", CultureInfo.InvariantCulture);
             var endDate = DateTime.ParseExact(Console.ReadLine(),
-                "dd.m.yyyy", CultureInfo.InvariantCulture);
+                "d.M.yyyy", CultureInfo.InvariantCulture);
             var holidaysCount = 0;
-            for (var date = startDate; date <= endDate; date.AddDays(1))
-                if (date.DayOfWeek == DayOfWeek.Saturday &&
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
                     date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
             Console.WriteLine(holidaysCount);
 
