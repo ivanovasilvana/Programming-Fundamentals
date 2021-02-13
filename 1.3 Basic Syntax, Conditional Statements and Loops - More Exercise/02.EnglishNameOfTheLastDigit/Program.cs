6d82c440b3a473db53122de7f58ab68105aec71b
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-13 15:54:44 +0200

    02. English Name of the Last Digit

---
+++ 1.3 Basic Syntax, Conditional Statements and Loops - More Exercise/02.EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _02.EnglishNameOfTheLastDigit
+{
+    class Program
+    {
+        static string NameOfLastDigits()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int digit = number % 10;
+            string name = string.Empty;
+            if (digit == 0)
+            {
+                name = "zero";
+            }
+            else if (digit == 1)
+            {
+                name = "one";
+            }
+            else if (digit == 2)
+            {
+                name = "two";
+            }
+            else if (digit == 3)
+            {
+                name = "three";
+            }
+            else if (digit == 4)
+            {
+                name = "four";
+            }
+            else if (digit == 5)
+            {
+                name = "five";
+            }
+            else if (digit == 6)
+            {
+                name = "six";
+            }
+            else if (digit == 7)
+            {
+                name = "seven";
+            }
+            else if (digit == 8)
+            {
+                name = "eight";
+            }
+            else if (digit == 9)
+            {
+                name = "nine";
+            }
+            return (name);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NameOfLastDigits());
+           
+        }
+    }
+}
