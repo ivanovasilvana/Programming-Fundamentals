c15d34545c941ddd096124ddda36ce7224055f17
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-13 14:56:01 +0200

    rename

---
+++ 1.2BasicSyntax,ConditionalStatementsAndLoops-Exercise/07.VendingMachine/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _07.VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+            double price = 0;
+
+            while (input != "Start")
+            {
+                double money = double.Parse(input);
+                if (money != 0.1 && money != 0.2 && money != 0.5 && money != 1 && money != 2  )
+                {
+                    Console.WriteLine($"Cannot accept {money}");
+                }
+                else
+                {
+                    sum += money;
+                }
+                input = Console.ReadLine();
+            }
+            string inputTwo = Console.ReadLine();
+
+            while (inputTwo != "End")
+            {
+                if (inputTwo != "Nuts" && inputTwo != "Water" && inputTwo != "Crisps" && inputTwo != "Soda" && inputTwo != "Coke")
+                {
+                    Console.WriteLine("Invalid product");
+                }
+                else
+                {
+                    if (inputTwo == "Nuts")
+                    {
+                        price += 2;
+                    }
+                    else if (inputTwo == "Water" )
+                    {
+                        price += 0.7;
+                    }
+                    else if (inputTwo == "Crisps")
+                    {
+                        price += 1.5;
+                    }
+                    else if (inputTwo == "Soda")
+                    {
+                        price += 0.8;
+                    }
+                    else if (inputTwo == "Coke")
+                    {
+                        price += 1;
+                    }
+                    if (price <= sum)
+                    {
+                        string newString = inputTwo.ToLower();
+                        Console.WriteLine($"Purchased {newString}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        break;
+                    }
+                }
+                inputTwo = Console.ReadLine();
+            }
+            double moneyLeft = Math.Abs(sum - price);
+            Console.WriteLine($"Change: {moneyLeft:f2}");
+        }
+    }
+}
---
+++ 1.2BasicSyntax,ConditionalStatementsAndLoops-Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceOfLightsaber = double.Parse(Console.ReadLine());
+            double priceOfRobe = double.Parse(Console.ReadLine());
+            double priceOfBelt = double.Parse(Console.ReadLine());
+
+            double countOfLightsaber = Math.Ceiling(countOfStudents + countOfStudents * 0.1);
+            double price = priceOfLightsaber * countOfLightsaber + priceOfRobe * countOfStudents + priceOfBelt * countOfStudents;
+
+            int counter = 0;
+
+            for (int i = 1; i <= countOfStudents; i++)
+            {
+                counter++;
+                if (counter == 6)
+                {
+                    price -= priceOfBelt;
+                    counter = 0;
+                }
+            }
+
+            if (price <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {price:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(price - money):f2}lv more.");
+            }
+        }
+    }
+}
