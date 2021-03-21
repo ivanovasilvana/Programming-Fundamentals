bc0e5abafb71e87f8b8091fc539bc6ca8b2d5e4b
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-03-21 17:30:41 +0200

    Arrays exc 1, 2

---
+++ 3.1Arrays - Lab/02PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [] num = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                Console.Write(num[i] + " ");
+            }
+        }
+    }
+}
