7e96f0005f965bba05ef5a73fb4ef9d264daa2fb
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 17:31:04 +0200

    Arrays Exc - 1 Train

---
+++ 3.2Arrays-Exercise/01Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+            int[] array = new int[countOfWagons];
+            int sum = 0;
+            for (int i = 0; i < countOfWagons; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                array[i] = people;
+                sum += people;
+            }
+            Console.WriteLine(string.Join(' ', array));
+            Console.WriteLine(sum);
+        }
+    }
+}
