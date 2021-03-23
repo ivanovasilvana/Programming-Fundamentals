0897d82131defaa3193fe4e1b35f2543a80775e2
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 16:10:36 +0200

    05. Sum Even Numbers

---
+++ 3.1Arrays - Lab/05SumEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split("\\s+").Select(int.Parse).ToArray();
+            int evenSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentNumber = array[i];
+                if (currentNumber % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+            }
+            Console.WriteLine(evenSum);
+        }
+    }
+}
