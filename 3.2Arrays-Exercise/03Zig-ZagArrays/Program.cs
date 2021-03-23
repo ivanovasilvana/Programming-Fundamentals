ae9a46002058601359f20740dd7108b9fbcc0a84
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 17:57:43 +0200

    02. Common Elements

---
+++ 3.2Arrays-Exercise/03Zig-ZagArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
