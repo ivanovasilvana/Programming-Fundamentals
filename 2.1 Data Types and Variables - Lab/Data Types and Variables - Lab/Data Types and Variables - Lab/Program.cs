3756638990f79b3dc232dc1fd10b0a664abc2801
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 15:05:14 +0200

    Fundamental Array-Lab - exc 1

---
+++ 2.1 Data Types and Variables - Lab/Data Types and Variables - Lab/Data Types and Variables - Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Data_Types_and_Variables___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pound = float.Parse(Console.ReadLine());
+            float US = 1.31f * pound;
+            Console.WriteLine($"{US:f3}");
+        }
+    }
+}
---
+++ 3.1Arrays - Lab/3.1Arrays - Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3._1Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int dayNum = int.Parse(Console.ReadLine());
+
+            if (dayNum < 1 || dayNum > days.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[dayNum - 1]);
+            }
+        }
+    }
+}
