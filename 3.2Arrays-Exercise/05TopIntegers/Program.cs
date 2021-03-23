d42843a8b3050c717292a0e153d94ba27463ee6e
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 20:22:18 +0200

    05. Top Integers

--- 3.2Arrays-Exercise/05TopIntegers/Program.cs
+++ 3.2Arrays-Exercise/05TopIntegers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05TopIntegers
 {
@@ -6,7 +7,27 @@ namespace _05TopIntegers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isBigger = true;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentInt = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] >= currentInt)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    Console.Write(currentInt + " ");
+                }
+                isBigger = true;
+
+            }
         }
     }
 }
