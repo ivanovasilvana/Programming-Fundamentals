c87a8728a673b63e875cfc8d1d8f6133c4f1ba93
Svetoslav-Ivanov <svetoslav.ivanov.eme@gmail.com>
2021-03-23 16:01:22 +0200

    04. Reverse Array of Strings

---
+++ 3.1Arrays - Lab/04ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            string[] newArray = new string[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[array.Length - i - 1] = array[i];
+            }
+            Console.WriteLine(string.Join(' ', newArray));
+        }
+    }
+}
