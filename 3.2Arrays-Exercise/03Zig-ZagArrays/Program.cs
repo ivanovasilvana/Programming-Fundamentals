92a157bdef2ea12285a6f903c9e82134a0ff55a8
ivanovasilvana <sisi_965@abv.bg>
2021-04-10 18:32:46 +0300

    Update

---
+++ 3.2Arrays-Exercise/03Zig-ZagArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] firstArray = new string[n];
+            string[] secondArray = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] items = Console.ReadLine().Split(' ').ToArray();
+
+                if (i % 2 != 0)
+                {
+                    firstArray[i] = items[1];
+                    secondArray[i] = items[0];
+                }
+                else
+                {
+                    firstArray[i] = items[0];
+                    secondArray[i] = items[1];
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', firstArray));
+            Console.WriteLine(string.Join(' ', secondArray));
+        }
+    }
+}
